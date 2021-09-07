e7c05ee2aab80efe598f1ff63a630af511aa8630
lizsjh <37032480+lizsjh@users.noreply.github.com>
2021-09-06 23:39:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const response=new Array();
 botui.message.add({
     delay:500,
     loading: true,
-    content: 'Hello! This is Alex, and I am a bot created by the customer service department.'
+    content: 'Hello. I am a customer service bot.'
 }).then(function(){
     return botui.message.add({
         delay:700,
         loading: true,
-        content:'Thank you for contacting us. I am delighted to handle your request today! Can you please tell me why you are here, such as delivery delay, incorrect order, etc.?'
+        content:'Thank you for contacting us. What brings you here'
     });
 }).then(function(){
     return botui.action.text({
@@ -25,7 +25,7 @@ botui.message.add({
     return botui.message.add({
         delay:900,
         loading: true,
-        content:'I can help you with that, and I am excited to do so! Could you tell me your order number?'
+        content:'I see your frustration. I can help you with that. Could you tell me your order number?'
     });
 }).then(function(){
     return botui.action.text({
@@ -36,12 +36,12 @@ botui.message.add({
     });
 }).then(function (res) { 
         console.log(res.value);
-        response.push(res.value); 
+        response.push(res.value);
 }).then(function(){
     return botui.message.add({
         delay:700,
         loading: true,
-        content:'Got it! Please allow me few seconds for pulling up your order.'
+        content:'Got it. Please allow me few seconds for pulling up your order.'
     });
 }).then(function(){
     return botui.message.add({
@@ -69,20 +69,20 @@ botui.message.add({
     return botui.message.add({
         delay:700,
         loading: true,
-        content:'Alright! I’ll be happy to process your request! Please give me a moment.'
+        content:'Alright. I’ll process your request. Please give me a moment.'
     });
 }).then(function(){
     return botui.message.add({
         delay:900,
         loading: true,
-        content:'I have processed your request, and I am glad that the issue is resolved!'
+        content:'I have processed your request. I really see your irritation, and I hope your issue has been addressed.'
     });
 }).then(function(){
     sendcomplete();
     return botui.message.add({
         delay:700,
         loading: true,
-        content:'Please contact us again if you need further assistance. Bye!'
+        content:'Please contact us again if you need further assistance. Bye.'
     });
 });
 
